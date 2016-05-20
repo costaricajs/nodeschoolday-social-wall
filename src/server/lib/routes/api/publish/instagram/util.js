48b037a1e14a8722c4ac592b5e13207a95710e9e
rubenabix <rubenabix@gmail.com>
2016-05-20 11:22:44 -0600

    fix subscription response

--- src/server/lib/routes/api/publish/instagram/util.js
+++ src/server/lib/routes/api/publish/instagram/util.js
@@ -10,9 +10,10 @@ function publish(options) {
     console.log('--------------------------');
     console.log('options.request.query');
     console.log(options.request.query);
-    
+
     if (options.input['hub.verify_token'] === verifyToken) {
       options.result = options.input['hub.challenge'];
+      console.log('options.result', options.result, 1);
       resolve(options);
     } else {
       options.error = 'Verify token incorrect';
